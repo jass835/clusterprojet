21141e3628bb98662540f5f0eea3d5359be650dd
jass835 <zaieterjass@gmail.com>
2024-04-20 15:15:22 +0200

    ajoutbonus

--- clust.py
+++ clust.py
@@ -1,5 +1,7 @@
-import csv
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def read_csv(file_path):
     """
@@ -9,30 +11,23 @@ def read_csv(file_path):
     file_path (str): Chemin vers le fichier CSV.
     
     Returns:
-    list de listes: Liste des points de données.
+    numpy.ndarray: Tableau numpy contenant les points de données.
     """
-    data = []
-    with open(file_path, 'r') as file:
-        reader = csv.reader(file)
-        next(reader)  # Sauter la première ligne (en-têtes)
-        for row in reader:
-            data.append([float(x) for x in row])
+    data = np.loadtxt(file_path, delimiter=',', skiprows=1)
     return data
-
-
 
 def initialize_centroids(data, k):
     """
     Fonction pour initialiser les centroïdes pour l'algorithme k-means.
     
     Args:
-    data (list de listes): Liste des points de données.
+    data (numpy.ndarray): Tableau numpy contenant les points de données.
     k (int): Nombre de clusters.
     
     Returns:
-    list de listes: Liste des centroïdes initialisés.
+    numpy.ndarray: Tableau numpy contenant les centroïdes initialisés.
     """
-    centroids = random.sample(data, k)
+    centroids = data[random.sample(range(len(data)), k)]
     return centroids
 
 def assign_points_to_clusters(data, centroids):
@@ -40,16 +35,73 @@ def assign_points_to_clusters(data, centroids):
     Fonction pour assigner les points aux clusters en fonction des centroïdes les plus proches.
     
     Args:
-    data (list de listes): Liste des points de données.
-    centroids (list de listes): Liste des centroïdes.
+    data (numpy.ndarray): Tableau numpy contenant les points de données.
+    centroids (numpy.ndarray): Tableau numpy contenant les centroïdes.
     
     Returns:
-    list: Liste des affectations de cluster pour chaque point.
+    numpy.ndarray: Tableau numpy contenant les affectations de cluster pour chaque point.
     """
-    clusters = []
-    for point in data:
-        distances = [sum((point[i] - centroid[i]) ** 2 for i in range(len(point))) for centroid in centroids]
-        cluster = distances.index(min(distances))
-        clusters.append(cluster)
-    return clusters
+    distances = np.sqrt(((data[:, np.newaxis] - centroids) ** 2).sum(axis=2))
+    cluster_assignments = np.argmin(distances, axis=1)
+    return cluster_assignments
 
+def calculate_quality_statistic(data, centroids, cluster_assignments):
+    """
+    Calculates the quality statistic for k-means clustering.
+
+    Args:
+    data (numpy.ndarray): Tableau numpy contenant les points de données.
+    centroids (numpy.ndarray): Tableau numpy contenant les centroïdes.
+    cluster_assignments (numpy.ndarray): Tableau numpy contenant les affectations de cluster pour chaque point.
+
+    Returns:
+    float: La moyenne des distances entre chaque point de données et son centroïde assigné.
+    """
+    total_distance = np.sum(np.sum((data[i] - centroids[cluster_assignments[i]]) ** 2) for i in range(len(data)))
+    quality_statistic = total_distance / len(data)
+    return quality_statistic
+
+def save_results_to_csv(data, centroids, cluster_assignments, output_file):
+    """
+    Sauvegarde les résultats de k-means dans un fichier CSV avec les coordonnées des centroides.
+    
+    Args:
+    data (numpy.ndarray): Tableau numpy contenant les points de données.
+    centroids (numpy.ndarray): Tableau numpy contenant les centroïdes.
+    cluster_assignments (numpy.ndarray): Tableau numpy contenant les affectations de cluster pour chaque point.
+    output_file (str): Chemin vers le fichier CSV de sortie.
+    """
+    data_with_centroids = np.hstack((data, centroids[cluster_assignments]))
+    np.savetxt(output_file, data_with_centroids, delimiter=',', header='x,y,centroid_x,centroid_y', comments='')
+
+# Bonus 1: Tracer le graphe de la distance moyenne en fonction du nombre de clusters
+def plot_average_distance(data, max_k):
+    distances = []
+    for k in range(1, max_k + 1):
+        centroids = initialize_centroids(data, k)
+        cluster_assignments = assign_points_to_clusters(data, centroids)
+        distance = calculate_quality_statistic(data, centroids, cluster_assignments)
+        distances.append(distance)
+    plt.plot(range(1, max_k + 1), distances)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Average Distance')
+    plt.title('Average Distance vs Number of Clusters')
+    plt.show()
+
+# Bonus 2: Animation montrant le découpage des données en clusters
+def animate_kmeans(data, frames):
+    fig = plt.figure()
+    ax = plt.axes(xlim=(np.min(data[:, 0]), np.max(data[:, 0])), ylim=(np.min(data[:, 1]), np.max(data[:, 1])))
+    scatter = ax.scatter(data[:, 0], data[:, 1], s=10)
+    centroids = frames[0]['centroids']
+    centroids_plot = ax.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='x')
+
+    def update(frame):
+        cluster_assignments = frame['cluster_assignments']
+        centroids = frame['centroids']
+        scatter.set_array(cluster_assignments)
+        centroids_plot.set_offsets(centroids)
+        return scatter, centroids_plot
+
+    anim = FuncAnimation(fig, update, frames=frames, interval=1000, blit=True)
+    plt.show()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# main.py
-
-from clust import read_csv, initialize_centroids, assign_points_to_clusters
+from clust import read_csv, initialize_centroids, assign_points_to_clusters, save_results_to_csv, calculate_quality_statistic, plot_average_distance, animate_kmeans
 from drawing import draw
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Chemin vers le fichier CSV
-file_path = '3d_data.csv'
+file_path = '2d_data.csv'
 
 # Nombre de clusters
 k = 10
@@ -18,7 +18,29 @@ centroids = initialize_centroids(data, k)
 # Assigner les points aux clusters
 cluster_assignments = assign_points_to_clusters(data, centroids)
 
+# Calculer la statistique de qualité de k-means
+quality_statistic = calculate_quality_statistic(data, centroids, cluster_assignments)
+print("La statistique de qualité de k-means est :", quality_statistic)
+
 # Afficher les données et leurs assignations de cluster
 draw(data, windowSize=800)
 
+# Sauvegarder les résultats de k-means dans un fichier CSV
+output_file = 'results.csv'
+save_results_to_csv(data, centroids, cluster_assignments, output_file)
+print("Les résultats de k-means ont été sauvegardés dans", output_file)
+
+# Bonus 1: Tracer le graphe de la distance moyenne en fonction du nombre de clusters
+plot_average_distance(data, max_k=10)
+
+# Bonus 2: Animation montrant le découpage des données en clusters
+frames = []
+max_iterations = 10
+for i in range(max_iterations):
+    cluster_assignments = assign_points_to_clusters(data, centroids)
+    frames.append({'centroids': centroids, 'cluster_assignments': cluster_assignments})
+    new_centroids = np.array([np.mean(data[cluster_assignments == j], axis=0) for j in range(k)])
+    centroids = new_centroids
+
+animate_kmeans(data, frames)
 
